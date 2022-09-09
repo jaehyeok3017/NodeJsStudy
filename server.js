fb4a3090d69e1d9b6d7e724ebb18330450cbca76
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-09-10 01:59:16 +0900

    Feat : `server.js`, `write.html` Request Post
    - 폼에 입력한 데이터가 서버에 전송되는 것까지

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({extended: true})); 
 
 app.listen(8080, () => {
     console.log('listening on 8080');
@@ -21,4 +21,10 @@ app.get('/pet', (req, res) => {
 
 app.get('/beauty', (req, res) => {
     res.send('뷰티 용품을 쇼핑할 수 있는 페이지입니다');
+});
+
+app.post('/add', (req, res) => {
+    res.send('전송완료')
+    console.log(req.body.title)
+    console.log(req.body.date)
 });
