1ca7bc4a322c2c1eab35939c171b19f7b47e34a1
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-09-10 01:24:24 +0900

    Minor : `server.js` function
    - 화살표로 변경

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.listen(8080, function(){
+app.use(express.urlencoded({extended: true})) 
+
+app.listen(8080, () => {
     console.log('listening on 8080');
 });
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/write', function(req, res){
+app.get('/write', (req, res) => {
     res.sendFile(__dirname + '/write.html');
 });
 
-app.get('/pet', function(req, res){
+app.get('/pet', (req, res) => {
     res.send('펫 용품을 쇼핑할 수 있는 페이지입니다');
 });
 
-app.get('/beauty', function(req, res){
+app.get('/beauty', (req, res) => {
     res.send('뷰티 용품을 쇼핑할 수 있는 페이지입니다');
 });
