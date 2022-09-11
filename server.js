32c3fb2c1f47bf1f639f9efbbc1c22be340db7cc
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-09-12 03:11:42 +0900

    Feat : `server.js`, `list.ejs` Create And Read `Complete
    - post로 받아온 것을 /list 경로에서 보여줌

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 
 let db;
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb+srv://gghh3017:test1234@cluster0.nvonzy2.mongodb.net/?retryWrites=true&w=majority', (error, client) => {
     if(error) return console.log(error);    
     db = client.db('todoapp');
-
-    db.collection('post').insertOne( {_id : 100, 이름 : 'Jaehyeok', 나이 : 17} , (error, result) => {
-        console.log('저장완료');
-    });
 
     app.listen(8080, () => {
         console.log('listening on 8080');
@@ -18,7 +15,7 @@ MongoClient.connect('mongodb+srv://gghh3017:test1234@cluster0.nvonzy2.mongodb.ne
     app.post('/add', (req, res) => {
         res.send('전송완료')
         
-        db.collection('post').insertOne( {_id : 101, 제목 : req.body.title, 날짜 : req.body.date} , (error, result) => {
+        db.collection('post').insertOne( {title : req.body.title, date : req.body.date} , (error, result) => {
             console.log('저장완료');
         });
     });
@@ -32,6 +29,13 @@ app.get('/', (req, res) => {
 
 app.get('/write', (req, res) => {
     res.sendFile(__dirname + '/write.html');
+});
+
+app.get('/list', (req, res) => {
+    db.collection('post').find().toArray((error, result) => {
+        console.log(result);
+        res.render('list.ejs', { posts : result });
+    });    
 });
 
 app.get('/pet', (req, res) => {
