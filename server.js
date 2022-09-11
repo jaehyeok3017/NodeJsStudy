107b6b616afde9b979450d6cb8e51e95bc2771df
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-09-11 23:33:39 +0900

    Feat : `server.js` title, date collection
    - title과 date를 /add에서 받아와서 저장하기

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 
+let db;
 const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb+srv://gghh3017:test1234@cluster0.nvonzy2.mongodb.net/?retryWrites=true&w=majority', (req, res) => {
-    // connect complet -> app.listen
+MongoClient.connect('mongodb+srv://gghh3017:test1234@cluster0.nvonzy2.mongodb.net/?retryWrites=true&w=majority', (error, client) => {
+    if(error) return console.log(error);    
+    db = client.db('todoapp');
+
+    db.collection('post').insertOne( {_id : 100, 이름 : 'Jaehyeok', 나이 : 17} , (error, result) => {
+        console.log('저장완료');
+    });
+
     app.listen(8080, () => {
         console.log('listening on 8080');
-    });    
+    });
+    
+    app.post('/add', (req, res) => {
+        res.send('전송완료')
+        
+        db.collection('post').insertOne( {_id : 101, 제목 : req.body.title, 날짜 : req.body.date} , (error, result) => {
+            console.log('저장완료');
+        });
+    });
 });
 
 app.use(express.urlencoded({extended: true})); 
@@ -25,10 +40,4 @@ app.get('/pet', (req, res) => {
 
 app.get('/beauty', (req, res) => {
     res.send('뷰티 용품을 쇼핑할 수 있는 페이지입니다');
-});
-
-app.post('/add', (req, res) => {
-    res.send('전송완료')
-    console.log(req.body.title)
-    console.log(req.body.date)
 });
